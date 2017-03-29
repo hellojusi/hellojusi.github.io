0aa81f71df56141d0041d7c6cb46ccd2897835a9
Justyna Ratajczak <justyna.ratajczak@gmail.com>
2017-03-29 16:54:46 +0200

    Added gulp, cleaned up template files

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+/**
+ * Require all the things!
+ */
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifyCSS = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	browserSync = require('browser-sync'),
+	cp = require('child_process');
+
+/**
+ * Set some path variables
+ */
+var base = './',
+	src = base + '_dev',
+	dist = base + 'assets',
+	paths = {
+		js: src + '/js/*.js',
+		scss: [ src +'/sass/*.scss',
+                src +'/sass/**/* .scss',
+                src +'/sass/**/**/*.scss'],
+        jekyll: ['index.html', '_posts/*', '_layouts/*', '_includes/*' , 'assets/*', 'assets/**/*', '*.md']
+	};
+
+
+/**
+ * Compile sass to css
+ */
+gulp.task('compile-sass', function() {
+	return gulp.src(paths.scss)
+		.pipe(sass())
+		.pipe(autoprefixer('last 3 versions', 'ie 9'))
+		.pipe(minifyCSS())
+		.pipe(rename( {dirname: dist + '/css', suffix: '.min'} ))
+		.pipe(gulp.dest('./'))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+// 
+// gulp.task('js', function() {
+// 	return gulp.src(paths.js)
+// 		.pipe()
+// });
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify('Building Jekyll');
+    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        },
+        host: "localhost"
+    });
+});
+
+/**
+ * Big Brother
+ */
+gulp.task('watch', function() {
+  gulp.watch(paths.scss, ['compile-sass']);
+  //gulp.watch(paths.js, ['js']);
+  gulp.watch(paths.jekyll, ['jekyll-rebuild']);
+});
+
+gulp.task('default', function() {
+    gulp.start('compile-sass', 'browser-sync', 'watch');
+});
